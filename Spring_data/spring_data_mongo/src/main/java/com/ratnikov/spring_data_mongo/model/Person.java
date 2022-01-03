de2891e66dbe7ff30a3bb5e90e39461d4fd8c7b7
titanmet <titanmet@mail.ru>
2022-01-03 20:46:02 +0300

    Spring Data MongoDb

--- Spring_data/spring_data_mongo/src/main/java/com/ratnikov/spring_data_mongo/model/Person.java
+++ Spring_data/spring_data_mongo/src/main/java/com/ratnikov/spring_data_mongo/model/Person.java
@@ -7,6 +7,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Data
 @Document(collection = "persons")
 public class Person {
+
     @Id
     private String id;
     private String name;
