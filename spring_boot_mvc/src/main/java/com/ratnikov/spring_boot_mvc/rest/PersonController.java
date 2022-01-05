0615b1268c7223d900bd748e4b31e5f11efeb346
titanmet <titanmet@mail.ru>
2022-01-05 09:05:38 +0300

    Spring Boot MVC ResponseEntity

--- spring_boot_mvc/src/main/java/com/ratnikov/spring_boot_mvc/rest/PersonController.java
+++ spring_boot_mvc/src/main/java/com/ratnikov/spring_boot_mvc/rest/PersonController.java
@@ -2,7 +2,7 @@ package com.ratnikov.spring_boot_mvc.rest;
 
 import com.ratnikov.spring_boot_mvc.model.Person;
 import com.ratnikov.spring_boot_mvc.repository.PersonRepository;
-import org.springframework.data.crossstore.ChangeSetPersister;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -27,6 +27,31 @@ public class PersonController {
     public PersonDto get(@PathVariable("id") long id) {
         Person person = personRepository.findById(id).orElseThrow(NotFoundException::new);
         return PersonDto.toDto(person);
+    }
+
+    @RequestMapping(value = "/person", method = RequestMethod.POST)
+    public @ResponseBody
+    PersonDto create(@RequestBody PersonDto dto) {
+        Person account = PersonDto.toDomainObject(dto);
+        Person accountWithId = personRepository.save(account);
+        return PersonDto.toDto(accountWithId);
+    }
+
+    @DeleteMapping(value = "/person/{id}")
+    public void delete(@PathVariable("id") long id) {
+        personRepository.deleteById(id);
+    }
+
+    @PutMapping(value = "/person/{id}/holder")
+    public void changeName(@PathVariable("id") long id, @RequestParam("name") String name) {
+        Person person = personRepository.findById(id).orElseThrow(NotFoundException::new);
+        person.setName(name);
+        personRepository.save(person);
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<String> handleNotFound(NotFoundException ex) {
+        return ResponseEntity.badRequest().body("Not found");
     }
 
 }
